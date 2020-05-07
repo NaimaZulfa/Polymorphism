1bef28ea9d94b6ce984a1d95b17818cc73550656
NaimaZulfa <naima.zulfa@students.amikom.ac.id>
2020-05-07 10:17:00 +0700

    inisialisasi project

---
+++ Polymorphism/PrinterWindows.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polymorphism
+{
+    class PrinterWindows
+    {
+        public string Name { get; set; }
+        public virtual void Show()
+        {
+            Console.WriteLine("Printer Windows display dimension: ");
+        }
+        public virtual void Print()
+        {
+            Console.WriteLine("Printer Windows printer printing....");
+        }
+    }
+}
---
+++ Polymorphism/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nama \t: Naima Zulfa");
+            Console.WriteLine("NIM \t: 19.11.2735");
+            Console.WriteLine("Kelas \t: IF 03\n");
+
+            PrinterWindows printer;
+            
+            Console.WriteLine("Pilih Printer: ");
+            Console.WriteLine("1. Epson");
+            Console.WriteLine("2. Canon");
+            Console.WriteLine("3. LaserJet\n");
+
+            Console.WriteLine("Nomor Printer [1..3]: ");
+            int nomorPrinter = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+
+            if (nomorPrinter == 1)
+                printer = new Epson();
+            else if (nomorPrinter == 2)
+                printer = new Canon();
+            else
+                printer = new LaserJet();
+
+            printer.Show();
+            printer.Print();
+
+            Console.ReadKey();
+        }
+    }
+}
